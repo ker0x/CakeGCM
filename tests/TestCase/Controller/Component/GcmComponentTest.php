61d9d8be424192c60c276f48be8e46179f21d79e
Scrutinizer Auto-Fixer <auto-fixer@scrutinizer-ci.com>
2016-03-21 19:55:38 +0000

    Scrutinizer Auto-Fixes
    This commit consists of patches automatically generated for this project on https://scrutinizer-ci.com

--- tests/TestCase/Controller/Component/GcmComponentTest.php
+++ tests/TestCase/Controller/Component/GcmComponentTest.php
@@ -11,38 +11,38 @@ use ker0x\CakeGcm\Controller\Component\GcmComponent;
 
 class GcmComponentTest extends TestCase
 {
-    public $component = null;
+	public $component = null;
 
-    public $controller = null;
+	public $controller = null;
 
-    public $ids = [];
+	public $ids = [];
 
 
-    public function setUp()
-    {
-        parent::setUp();
-         // Setup our component and fake test controller
-        $request = new Request();
-        $response = new Response();
-        $this->controller = $this->getMock(
-            'Cake\Controller\Controller',
-            null,
-            [$request, $response]
-        );
-        $registry = new ComponentRegistry($this->controller);
-        $this->component = new GcmComponent($registry);
-    }
+	public function setUp()
+	{
+		parent::setUp();
+		 // Setup our component and fake test controller
+		$request = new Request();
+		$response = new Response();
+		$this->controller = $this->getMock(
+			'Cake\Controller\Controller',
+			null,
+			[$request, $response]
+		);
+		$registry = new ComponentRegistry($this->controller);
+		$this->component = new GcmComponent($registry);
+	}
 
-    public function testSendError()
-    {
-        $isSend = $this->component->send($this->ids);
-        $this->assertEquals(new Exception(), $isSend);
-    }
+	public function testSendError()
+	{
+		$isSend = $this->component->send($this->ids);
+		$this->assertEquals(new Exception(), $isSend);
+	}
 
-    public function tearDown()
-    {
-        parent::tearDown();
-        // Clean up after we're done
-        unset($this->component, $this->controller);
-    }
+	public function tearDown()
+	{
+		parent::tearDown();
+		// Clean up after we're done
+		unset($this->component, $this->controller);
+	}
 }
