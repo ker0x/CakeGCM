0abce404f522137ed0a78d1e8918c9e7494572a3
Romain <monteil.romain@gmail.com>
2016-04-28 20:24:07 +0200

    move all logic inside a separate class Gcm.php

---
+++ tests/TestCase/Lib/GcmTest.php
@@ -0,0 +1,97 @@
+<?php
+namespace ker0x\CakeGcm\Test\TestCase\Lib;
+
+use Cake\TestSuite\IntegrationTestCase;
+use ker0x\CakeGcm\Lib\Gcm;
+
+class GcmTest extends IntegrationTestCase
+{
+    public $gcm = null;
+
+    public $tokens = null;
+
+    public function setUp()
+    {
+        parent::setUp();
+        // Setup our component and fake test controller
+        $this->gcm = new Gcm([
+            'api' => [
+                'key' => getenv('GCM_API_KEY')
+            ],
+            'http' => [
+                'ssl_verify_peer' => false,
+                'ssl_verify_peer_name' => false,
+                'ssl_verify_host' => false
+            ]
+        ]);
+        $this->tokens = getenv('TOKEN');
+    }
+
+    public function testSendNotification()
+    {
+        $result = $this->gcm->sendNotification(
+            $this->tokens,
+            [
+                'title' => 'Hello World',
+                'body' => 'My awesome Hello World!'
+            ],
+            [
+                'dry_run' => true
+            ]
+        );
+
+        $this->assertTrue($result);
+    }
+
+    public function testSendData()
+    {
+        $result = $this->gcm->sendData(
+            $this->tokens,
+            [
+                'data-1' => 'Lorem ipsum',
+                'data-2' => 1234,
+                'data-3' => true
+            ],
+            [
+                'dry_run' => true
+            ]
+        );
+
+        $this->assertTrue($result);
+    }
+
+    public function testResponse()
+    {
+        $this->gcm->send(
+            $this->tokens,
+            [
+                'notification' => [
+                    'title' => 'Hello World',
+                    'body' => 'My awesome Hello World!'
+                ],
+                'data' => [
+                    'data-1' => 'Lorem ipsum',
+                    'data-2' => 1234,
+                    'data-3' => true
+                ]
+            ],
+            [
+                'delay_while_idle' => 'true',
+                'time_to_live' => '60',
+                'dry_run' => 'true'
+            ]
+        );
+
+        $response = $this->gcm->response();
+
+        $this->assertEquals(1, $response['success']);
+        $this->assertEquals(0, $response['failure']);
+    }
+
+    public function tearDown()
+    {
+        parent::tearDown();
+        // Clean up after we're done
+        unset($this->gcm, $this->tokens);
+    }
+}
